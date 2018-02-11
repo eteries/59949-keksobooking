33a3bfe95562f295d98b79eefc9620443f8bd101
Funky-Juice <isa86box@gmail.com>
2018-02-11 22:03:57 +0300

    переписал метод

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-function prompt(question) {
-  console.log(question);
+function prompt() {
+  var args = process.argv.slice(2)
 
-  process.stdin.once('data', function (data) {
-    var command = data.toString().trim();
+  switch(args[0]) {
+    case '--version':
+      console.log('ver0.0.1');
+      process.exit()
 
-    switch(command) {
-      case '--version':
-        console.log('ver0.0.1');
-        process.exit()
+    case '--help':
+      console.log('Доступные команды:\n' +
+        '--help    — печатает этот текст;\n' +
+        '--version — печатает версию приложения;');
+      process.exit()
 
-      case '--help':
-        console.log('Доступные команды:\n' +
-          '--help    — печатает этот текст;\n' +
-          '--version — печатает версию приложения;');
-        process.exit()
+    case undefined:
+      console.log('Привет пользователь!\n' +
+        'Эта программа будет запускать сервер «DevSrv».\n' +
+        'Автор: Juice.');
+      process.exit()
 
-      case '':
-        console.log('Привет пользователь!\n' +
-          'Эта программа будет запускать сервер «DevSrv».\n' +
-          'Автор: Juice.');
-        process.exit()
-
-      default:
-        console.error('Неизвестная команда! "' + command + '"\n' +
-          'Чтобы прочитать правила использования приложения, наберите "--help".');
-        process.exit(1)
-    }
-  });
+    default:
+      console.error('Неизвестная команда! "' + args[0] + '"\n' +
+        'Чтобы прочитать правила использования приложения, наберите "--help".');
+      process.exit(1)
+  };
 }
 
-prompt('Жду команду...');
+prompt();
