c8780acdb5dfa51c9157106bd3e77ae296d4e90f
Funky-Juice <isa86box@gmail.com>
2018-02-12 16:44:43 +0300

    настроил проект

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    node: true,
+    commonjs: true,
+    browser: false,
+    mocha: true
+  },
+  extends: 'htmlacademy/node'
+};
