eb45ef1d15d5d284cc5bf80228ac8733edf3cfc4
Funky-Juice <isa86box@gmail.com>
2018-02-13 15:17:40 +0300

    раскрасил вывод в консоль

--- src/license.js
+++ src/license.js
@@ -1,9 +1,10 @@
 const packageInfo = require(`../package.json`);
+require(`colors`);
 
 module.exports = {
   name: `license`,
   description: `shows program license`,
   execute() {
-    console.log(packageInfo.license);
+    console.log(packageInfo.license.blue);
   }
 };
--- src/version.js
+++ src/version.js
@@ -1,9 +1,10 @@
-const packageInfo = require(`../package.json`);
+const packageVer = require(`../package.json`).version;
+require(`colors`);
 
 module.exports = {
   name: `version`,
   description: `shows program version`,
   execute() {
-    console.log(`v${packageInfo.version}`);
+    console.log(`v` + packageVer[0].red + `.` + packageVer[2].green + `.` + packageVer[4].blue);
   }
 };
