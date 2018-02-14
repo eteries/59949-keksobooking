f04465ab099833c6f9f1805baa42334f48381e2d
Иншаков Сергей <inshakovsa@edu.mos.ru>
2018-02-14 14:27:10 +0300

    исправления

--- src/help.js
+++ src/help.js
@@ -5,10 +5,10 @@ module.exports = {
   description: `shows program commands`,
   execute() {
     console.log(`Доступные команды:\n` +
-      `--help    — `.grey + ` печатает этот текст;\n`.green +
-      `--version — `.grey + ` печатает версию приложения;\n`.green +
-      `--author — `.grey + ` указывает автора приложения;\n`.green +
-      `--description — `.grey + ` выдает описание приложения;\n`.green +
-      `--license — `.grey + ` выводит тип лицензии;\n`.green);
+      `${`--help — `.grey} ${`печатает этот текст;\n`.green}` +
+      `${`--version — `.grey} ${`печатает версию приложения;\n`.green}` +
+      `${`--author — `.grey} ${`указывает автора приложения;\n`.green}` +
+      `${`--description — `.grey} ${`выдает описание приложения;\n`.green}` +
+      `${`--license — `.grey} ${`выводит тип лицензии;\n`.green}`);
   }
 };
