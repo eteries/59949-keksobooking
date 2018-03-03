09ae87d940f1cdafe74fa54854eec6e1bdc299e5
Funky-Juice <isa86box@gmail.com>
2018-03-03 15:36:19 +0300

    linter error fix

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
-const multer= require(`multer`);
+const multer = require(`multer`);
 const {generateEntity} = require(`./generator`);
 const app = express();
 
